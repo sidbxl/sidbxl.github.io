cd549aaebaa3fbbb62f3cd69e032b674f8b945be
Sid <11022440+sidbxl@users.noreply.github.com>
2025-05-19 16:37:42 +0200

    Fixing sw path in scripts

--- tarot/gallery_script.js
+++ tarot/gallery_script.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('sw.js').then(registration => {
+    navigator.serviceWorker.register('/tarot/sw.js').then(registration => {
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }).catch(error => {
       console.log('ServiceWorker registration failed: ', error);
